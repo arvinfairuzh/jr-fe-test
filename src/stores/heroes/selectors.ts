c1d7413a8e59f9f5c2f820bee51d91f8de85390a
arvinfairuzh <49090766+arvinfairuzh@users.noreply.github.com>
2019-03-30 15:33:21 +0700

    Add files via upload

---
+++ src/stores/heroes/selectors.ts
@@ -0,0 +1,17 @@
+import * as Heroes from "~/interfaces/heroes";
+
+export function getHeroesLoading(store: Heroes.HeroesState) {
+  return store.loading;
+}
+
+export function getHeroesIndex(store: Heroes.HeroesState) {
+  return store.index;
+}
+
+export function getHeroesData(store: Heroes.HeroesState) {
+  return store.data;
+}
+
+export function getHeroesErrors(store: Heroes.HeroesState) {
+  return store.errors || undefined;
+}
